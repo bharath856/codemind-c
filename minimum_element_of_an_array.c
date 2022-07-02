c35cb2a88e4448830e2508987ba9249eb3ad023f
bharath856 <99651027+bharath856@users.noreply.github.com>
2022-07-02 15:12:51 +0530

    #include<stdio.h> int main(){     int n,i,max=10000000;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(max>=arr[i])         {             max=arr[i];         }     }     printf("%d",max); }

--- minimum_element_of_an_array.c
+++ minimum_element_of_an_array.c
@@ -1,20 +1,18 @@
 #include<stdio.h>
-int main()
-{
-	int v[2004],k,l,r;
-	scanf("%d",&l);	
-	for(k=0;k<l;k++)
-	{
-		scanf("%d ",&v[k]);
-	}
-	r=v[0];
-	for(k=0;k<l;k++)
-	{
-		if(v[k]<=r)
-		{
-		    r=v[k];
-		}
-	}
-	printf("%d",r);
-	return 0;
+int main(){
+    int n,i,max=10000000;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(max>=arr[i])
+        {
+            max=arr[i];
+        }
+    }
+    printf("%d",max);
 }
