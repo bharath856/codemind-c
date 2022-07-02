1e5256a424010a529a55c5b3dcaca3c2a9060730
bharath856 <99651027+bharath856@users.noreply.github.com>
2022-07-02 15:23:31 +0530

    #include<stdio.h> int main(){     int n,i,max=0,k;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     scanf("%d",&k);     for(i=0;i<n;i++)     {         if(k==arr[i])         {             max++;         }     }     if(max!=0)     {         printf("True");     }     else     {         printf("False");     } }

--- element_search_in_an_array.c
+++ element_search_in_an_array.c
@@ -1,28 +1,26 @@
 #include<stdio.h>
-int main()
-{
-	int v[2004],k,r,l,m=0;
-	scanf("%d",&l);	
-	for(k=0;k<l;k++)
-	{
-		scanf("%d ",&v[k]);
-	}
-	scanf("%d",&r);
-	for(k=0;k<l;k++)
-	{
-		if(v[k]==r)
-		{
-			m++;
-			break;
-		}
-	}
-	if(m==1)
-	{
-		printf("True");
-	}
-	else
-	{
-		printf("False");
-	}
-	return 0;
+int main(){
+    int n,i,max=0,k;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    scanf("%d",&k);
+    for(i=0;i<n;i++)
+    {
+        if(k==arr[i])
+        {
+            max++;
+        }
+    }
+    if(max!=0)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
 }
