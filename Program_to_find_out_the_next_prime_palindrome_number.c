8868e1e3e4f53c1e77e19dc47e4d91e3c67957b8
bharath856 <99651027+bharath856@users.noreply.github.com>
2022-10-12 10:17:49 +0530

    #include<stdio.h> int isitpal(int num) {     int d,res=0,temp;     temp=num;     while(num)     {         d=num%10;         res=res*10+d;         num=num/10;     }     if(temp==res )     {         return 1;     }     else     {         return 0;     } } int isprime(int num) {     int i,fc=0;     for(i=1;i<=num;i++) 	{ 		if(num%i==0) 		{ 			fc++; 		} 	} 	if(fc==2) 	{ 		return 1; 	} 	else 	{ 		return 0; 	} }
    int main()
    {
        int n,np;
        scanf("%d",&n);
        for(np=n+1;;np++)
        {
            if(isitpal(np)&&isprime(np))
            {
                break;
            }
        }
        printf("%d",np);
    }

--- Program_to_find_out_the_next_prime_palindrome_number.c
+++ Program_to_find_out_the_next_prime_palindrome_number.c
@@ -1,39 +1,53 @@
 #include<stdio.h>
+int isitpal(int num)
+{
+    int d,res=0,temp;
+    temp=num;
+    while(num)
+    {
+        d=num%10;
+        res=res*10+d;
+        num=num/10;
+    }
+    if(temp==res )
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+int isprime(int num)
+{
+    int i,fc=0;
+    for(i=1;i<=num;i++)
+	{
+		if(num%i==0)
+		{
+			fc++;	
+		}	
+	}
+	if(fc==2)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
 int main()
 {
-    int n,i,k,c=0,x=0,z=0,d=0,res=0;
+    int n,np;
     scanf("%d",&n);
-    k=n+1;
-    while(c<=1)
+    for(np=n+1;;np++)
     {
-        x=0;
-        for(i=1;i<=k;i++)
+        if(isitpal(np)&&isprime(np))
         {
-            if(k%i==0)
-            {
-                x+=1;
-            }
+            break;
         }
-        if(x==2)
-        {
-           res=0;
-           z=0;
-           z=k;
-           d=0;
-           while(z!=0)
-           {
-               d=z%10;
-               z=z/10;
-               res=res*10+d;
-           }
-           if(res==k)
-           {
-               c+=1;
-               break;
-           }
-        }
-        k+=1;
     }
-    printf("%d",k);
-    return 0;
+    printf("%d",np);
 }
